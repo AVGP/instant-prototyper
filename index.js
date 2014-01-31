8ba94b5450c864b47536b5caceef30f36ce54909
Martin N <mr.avgp@googlemail.com>
2014-01-31 13:38:17 -0500

    Here it is. The precious server.

---
+++ index.js
@@ -0,0 +1,41 @@
+var app     = require('http').createServer(handler),
+    io      = require('socket.io').listen(app),
+    fs      = require('fs'),
+    watcher = require('chokidar').watch(process.argv[1]),
+    clients = [];
+    
+    
+var prototyperShellHTML = fs.readFileSync(__dirname + '/www/index.html');
+
+
+watcher.on("change", function(path) {
+  console.log(path);
+  for(var i=0;i<clients.length;i++) {
+    clients[i].emit("update", path);
+  }
+});
+
+app.listen(process.env.PORT | 8080);
+
+function handler (req, res) {
+
+    if(req.url == "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.end(prototyperShellHTML);
+    } else {
+        fs.readFile(req.url.substr(1), function(err, data) {
+           if(err) {
+               res.writeHead(404);
+               res.end("File not found: " + req.url);
+               return;
+           }
+           
+           res.writeHead(200);
+           res.end(data);
+        });
+    }
+}
+
+io.sockets.on('connection', function (socket) {
+  clients.push(socket);
+});
